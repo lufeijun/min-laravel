a98620e9d6c73db0908f3f5ad61cc19d4b1405fd
lufeijun1234 <lufeijun_1234@126.com>
2020-04-18 13:13:33 +0800

    搭建框架目录结构

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * min-Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  min-Laravel
+ * @author   lufeijun1234 <lufeijun_1234@126.com>
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/**
+ * 引入自动加载文件
+ */
+require __DIR__.'/../vendor/autoload.php';
+
+
+
+show('hello world');
